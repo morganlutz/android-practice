4b5b6caa63f894e6a2c3d3b7223103f1d19f5a02
Morgan Lutz <morgan.lutz@gmail.com>
2015-10-22 10:01:51 -0700

    retrieves xml data

--- Top10Downloader/app/src/main/java/com/toggleable/morgan/top10downloader/MainActivity.java
+++ Top10Downloader/app/src/main/java/com/toggleable/morgan/top10downloader/MainActivity.java
@@ -7,6 +7,7 @@ import android.support.v7.widget.Toolbar;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import java.io.IOException;
 import java.io.InputStream;
@@ -16,6 +17,10 @@ import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
 
+    private TextView xmlTextView;
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -23,6 +28,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        xmlTextView = (TextView) findViewById(R.id.xmlTextView);
         DownloadData downloadData = new DownloadData();
         downloadData.execute("http://ax.itunes.apple.com/WebObjects/MZStoreServices.woa/ws/RSS/topfreeapplications/limit=10/xml");
 
@@ -76,6 +82,7 @@ public class MainActivity extends AppCompatActivity {
         protected void onPostExecute(String result) {
             super.onPostExecute(result);
             Log.d("DownloadData", "Result was " + result);
+            xmlTextView.setText(mFileContents);
         }
 
         private String downloadXMLFile(String urlPath) {
@@ -102,6 +109,7 @@ public class MainActivity extends AppCompatActivity {
 
             } catch(IOException e) {
                 Log.d("DownloadData", "IO Exception reading data: " + e.getMessage());
+//                e.printStackTrace();
             } catch(SecurityException e) {
                 Log.d("DownloadData", "Security exception. Needs permissions? " + e.getMessage());
             }
