7bdf255dfa569399cf027834b39721e5c08600b8
Morgan Lutz <morgan.lutz@gmail.com>
2015-10-22 16:36:50 -0700

    refactor

--- HelloWorld/learnjava/src/main/java/com/example/Demo.java
+++ HelloWorld/learnjava/src/main/java/com/example/Demo.java
@@ -2,6 +2,7 @@ package com.example;
 
 public class Demo {
     public static void main(String[] args) {
-        System.out.println("Hello World");
+        String testMessage = "This is a test";
+        System.out.println(testMessage);
     }
 }
