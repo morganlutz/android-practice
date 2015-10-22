00c58c458a2f8991e3e455e30987fa9a5548bef5
Morgan Lutz <morgan.lutz@gmail.com>
2015-10-22 13:42:35 -0700

    initial file dump

---
+++ YoutubeApp/app/src/main/java/com/toggleable/morgan/youtubeapp/StandaloneActivity.java
@@ -0,0 +1,58 @@
+package com.toggleable.morgan.youtubeapp;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Button;
+
+public class StandaloneActivity extends AppCompatActivity implements View.OnClickListener {
+
+    private String GOOGLE_API_KEY = "";
+    private String YOUTUBE_VIDEO_ID = "";
+    private String YOUTUBE_PLAYLIST_ID = "";
+
+    private Button btnPlayVideo;
+    private Button btnPlaylist;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_standalone);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        btnPlayVideo = (Button) findViewById(R.id.btnPlayVideo);
+        btnPlaylist = (Button) findViewById(R.id.btnPlaylist);
+        btnPlayVideo.setOnClickListener(this);
+        btnPlaylist.setOnClickListener(this);
+
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent intent = null;
+        switch(v.getId()) {
+            case R.id.btnPlayVideo:
+                intent = YouTubeStandalonePlayer.createVideoIntent(this, GOOGLE_API_KEY, YOUTUBE_VIDEO_ID);
+                break;
+            case R.id.btnPlaylist:
+                intent = YouTubeStandalonePlayer.createPlaylistIntent(this, GOOGLE_API_KEY, YOUTUBE_PLAYLIST_ID);
+                break;
+            default:
+        }
+        if(intent != null) {
+            startActivity(intent);
+        }
+    }
+
+}
---
+++ YoutubeApp/app/src/main/java/com/toggleable/morgan/youtubeapp/YoutubeActivity.java
@@ -0,0 +1,104 @@
+package com.toggleable.morgan.youtubeapp;
+
+import android.os.Bundle;
+import android.widget.Toast;
+
+public class YoutubeActivity implements YouTubePlayer.OnInitializedListener {
+
+    private String GOOGLE_API_KEY = "TBA";
+    private String YOUTUBE_VIDEO_ID = "TBA";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_youtube);
+        YouTubePlayerView youTubePlayerView = (YouTubePlayerView) findViewById(R.id.youtube_player);
+        youTubePlayerView.initialize(GOOGLE_API_KEY, this);
+        //generate override methods for onInitializationSuccess
+        // change boolean b to boolean wasRestored
+        //youTubePlayer.setPlayerStateChangeListener(playerStateChangeListener);
+        //youTubePlayer.setPlaybackEventListener(playbackEventListener);
+        //if(!wasRestored) {
+        // youTubePlayer.cueVideo(YOUTUBE_VIDEO_ID);
+        //}
+        // && onInitializationFailure :
+        //add Toast.makeText(this, "Cannot initialize Youtube player",
+        //Toast.LENGTH_LONG).show();
+
+        private YouTubePlayer.PlaybackEventListener playbackEventListener = new YouTubePlayer.PlaybackEventListener() {
+            @Override
+            public void onPlaying() {
+                Toast.makeText(YoutubeActivity.this, "Good, video is playing...", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onPaused() {
+                Toast.makeText(YoutubeActivity.this, "Oh you paused??", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onStopped() {
+
+            }
+
+            @Override
+            public void onBuffering(boolean b) {
+
+            }
+
+            @Override
+            public void onSeekTo(int i) {
+
+            }
+        };
+
+        private YouTubePlayer.PlayerStateChangeListener playerStateChangeListener = new YouTubePlayer.PlayerStateChangeListener() {
+            @Override
+            public void onLoading() {
+
+            }
+
+            @Override
+            public void onLoaded(String s) {
+
+            }
+
+            @Override
+            public void onAdStarted() {
+                Toast.makeText(YoutubeActivity.this, "Make the video content author rich??", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onVideoStarted() {
+                Toast.makeText(YoutubeActivity.this, "Video started!", Toast.LENGTH_LONG).show();
+
+            }
+
+            @Override
+            public void onVideoEnded() {
+
+            }
+
+            @Override
+            public void onError(YouTubePlayer.ErrorReason errorReason) {
+
+            }
+        };
+
+//        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+//        setSupportActionBar(toolbar);
+
+//        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+//        fab.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+//                        .setAction("Action", null).show();
+//            }
+//        });
+    }
+
+}
