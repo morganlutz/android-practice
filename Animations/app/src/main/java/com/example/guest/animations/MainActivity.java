fb9dc9887513765358c4ab54897fd6cf7aba1ccf
Guest User <student@epicodus.com>
2015-10-27 11:42:44 -0700

    add initial files

---
+++ Animations/app/src/main/java/com/example/guest/animations/MainActivity.java
@@ -0,0 +1,56 @@
+package com.example.guest.animations;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
+
+public class MainActivity extends AppCompatActivity {
+
+
+
+    //crossfade examples
+    public void fadeSpace1(View view) {
+        ImageView space1 = (ImageView) findViewById(R.id.space1);
+        ImageView space2 = (ImageView) findViewById(R.id.space2);
+        space1.animate().alpha(0f).setDuration(2000);
+        space2.animate().alpha(1f).setDuration(2000);
+    }
+
+    public void fadeSpace2(View view) {
+        ImageView space1 = (ImageView) findViewById(R.id.space1);
+        ImageView space2 = (ImageView) findViewById(R.id.space2);
+        space2.animate().alpha(0f).setDuration(2000);
+        space1.animate().alpha(1f).setDuration(2000);
+    }
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
