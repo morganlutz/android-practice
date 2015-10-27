09f12a8eeb13aaf9dc135f0af22bfc1be8e07621
Guest User <student@epicodus.com>
2015-10-27 14:41:08 -0700

    end game with msg and button

--- TicTacToe/app/src/main/java/com/example/guest/tictactoe/MainActivity.java
+++ TicTacToe/app/src/main/java/com/example/guest/tictactoe/MainActivity.java
@@ -15,6 +15,8 @@ public class MainActivity extends AppCompatActivity {
     int[] gameState = {2, 2, 2, 2, 2, 2, 2, 2, 2};
     //2 means unplayed
 
+    int[][] winningPositions = { {0,1,2}, {3,4,5}, {6,7,8}, {0,3,6}, {1,4,7}, {2,5,8}, {0,4,8}, {2,4,6}};
+
     public void dropIn(View view) {
 
         ImageView counter = (ImageView) view;
@@ -37,6 +39,14 @@ public class MainActivity extends AppCompatActivity {
                 activePlayer = 0;
             }
             counter.animate().translationYBy(1000f).rotation(720).setDuration(300);
+
+            for (int[] winningPosition : winningPositions) {
+                if(gameState[winningPosition[0]] == gameState[winningPosition[1]] &&
+                        gameState[winningPosition[1]] == gameState[winningPosition[2]] &&
+                        gameState[winningPosition[0]] != 2) {
+                    System.out.println(gameState[winningPosition[0]]);
+                }
+            }
         }
     }
 
