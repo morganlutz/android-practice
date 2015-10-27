27a6b5f7452519f9338a96a67b8ad655767a5739
Guest User <student@epicodus.com>
2015-10-27 16:19:44 -0700

    add video

--- VideoDemo/app/src/main/java/com/example/guest/videodemo/MainActivity.java
+++ VideoDemo/app/src/main/java/com/example/guest/videodemo/MainActivity.java
@@ -4,6 +4,8 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.VideoView;
+import android.widget.MediaController
 
 public class MainActivity extends AppCompatActivity {
 
@@ -11,6 +13,15 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        VideoView video = (VideoView) findViewById(R.id.videoView);
+        video.setVideoPath("android.resource://" + getPackageName() + "/" + R.raw.demovideo);
+
+        MediaController mediaController = new MediaController(this);
+
+        mediaController.setAnchorView(video);
+
+        video.start();
     }
 
 
