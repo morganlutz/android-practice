612c6ef2ba843370a7361dee4c8d882610723d03
Guest User <student@epicodus.com>
2015-10-29 16:47:55 -0700

    fix map bug

--- PostmatesClone/app/src/main/java/com/toggleable/morgan/postmatesclone/ui/MapActivity.java
+++ PostmatesClone/app/src/main/java/com/toggleable/morgan/postmatesclone/ui/MapActivity.java
@@ -1,32 +1,16 @@
 package com.toggleable.morgan.postmatesclone.ui;
 
-import com.google.android.gms.maps.*;
-import com.google.android.gms.maps.model.*;
-
-import android.Manifest;
-import android.app.Activity;
-import android.content.pm.PackageManager;
+import android.content.Context;
 import android.location.Criteria;
-import android.location.LocationManager;
-import android.os.Bundle;
-
-
 import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
-import android.support.v4.app.FragmentActivity;
 import android.util.Log;
-import android.widget.Toast;
 
-import com.google.android.gms.common.ConnectionResult;
-import com.google.android.gms.common.api.GoogleApiClient;
-import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
-import com.google.android.gms.location.LocationServices;
-import com.google.android.gms.common.SupportErrorDialogFragment;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
-import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
@@ -34,84 +18,98 @@ import com.toggleable.morgan.postmatesclone.R;
 
 import static com.google.android.gms.common.GooglePlayServicesUtil.isGooglePlayServicesAvailable;
 
-public class MapActivity extends FragmentActivity implements
-        OnMapReadyCallback,
-        GoogleApiClient.ConnectionCallbacks,
-        GoogleApiClient.OnConnectionFailedListener,
-        LocationListener {
+public class MapActivity extends FragmentActivity implements android.location.LocationListener {
 
-    private MapActivity mMap; // Might be null if Google Play services APK is not available.
-    private static final String TAG = "MOO";
-    GoogleMap map;
+    private GoogleMap mMap; // Might be null if Google Play services APK is not available.
 
+    LocationManager locationManager;
+    String provider;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google_map);
-        SupportMapFragment supportMapFragment =
-                (SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map);
-        map = supportMapFragment.getMap();
-        map.setMyLocationEnabled(true);
-        getCurrentLocation();
+        setUpMapIfNeeded();
 
-    }
+        locationManager = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
 
-    public void getCurrentLocation() {
-        Location myLocation = map.getMyLocation();
-        if (myLocation != null) {
-            double dLatitude = myLocation.getLatitude();
-            double dLongitude = myLocation.getLongitude();
-            Log.i("APPLICATION", " : " + dLatitude);
-            Log.i("APPLICATION", " : " + dLongitude);
-            map.addMarker(new MarkerOptions().position(
-                    new LatLng(dLatitude, dLongitude)).title("My Location"));
-            map.moveCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(dLatitude, dLongitude), 16));
+        provider = locationManager.getBestProvider(new Criteria(), false);
 
-        } else {
-            Toast.makeText(this, "Unable to fetch the current location", Toast.LENGTH_SHORT).show();
+        Location location = locationManager.getLastKnownLocation(provider);
+
+        if (location != null) {
+
+            onLocationChanged(location);
+
         }
 
-    }
-
-
-    @Override
-    public void onMapReady(GoogleMap map) {
-
-//        double currentLatitude = location.getLatitude();
-//        double currentLongitude = location.getLongitude();
-        LatLng sydney = new LatLng(-33.867, 151.206);
-
-        map.setMyLocationEnabled(true);
-        map.moveCamera(CameraUpdateFactory.newLatLngZoom(sydney, 13));
-
-        map.addMarker(new MarkerOptions()
-                .title("Sydney")
-                .snippet("The most populous city in Australia.")
-                .position(sydney));
-    }
-
-
-
-    @Override
-    public void onConnected(Bundle bundle) {
 
     }
 
     @Override
-    public void onConnectionSuspended(int i) {
+    protected void onResume() {
+        super.onResume();
+        setUpMapIfNeeded();
+
+        locationManager.requestLocationUpdates(provider, 400, 1, this);
+    }
+
+
+    private void setUpMapIfNeeded() {
+        // Do a null check to confirm that we have not already instantiated the map.
+        if (mMap == null) {
+            // Try to obtain the map from the SupportMapFragment.
+            mMap = ((SupportMapFragment) getSupportFragmentManager().findFragmentById(R.id.map))
+                    .getMap();
+            // Check if we were successful in obtaining the map.
+            if (mMap != null) {
+                setUpMap();
+            }
+        }
+    }
+
+
+    private void setUpMap() {
 
     }
 
     @Override
     public void onLocationChanged(Location location) {
 
+        Double lat = location.getLatitude();
+        Double lng = location.getLongitude();
+
+        mMap.clear();
+
+        mMap.addMarker(new MarkerOptions().position(new LatLng(lat, lng)).title("Your location"));
+
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(new LatLng(lat, lng), 18));
+
     }
 
     @Override
-    public void onConnectionFailed(ConnectionResult connectionResult) {
+    public void onStatusChanged(String provider, int status, Bundle extras) {
 
     }
+
+    @Override
+    public void onProviderEnabled(String provider) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String provider) {
+
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        locationManager.removeUpdates(this);
+
+    }
+
 }
 
 
