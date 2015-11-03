10c042ee8b53ccbfb31e40bccffc4fd5294f2da8
Morgan Lutz <morgan.lutz@gmail.com>
2015-11-02 22:51:06 -0800

    todo app with firebase

---
+++ ToDoApp/app/src/main/java/com/toggleable/morgan/todoapp/MainActivity.java
@@ -0,0 +1,116 @@
+package com.toggleable.morgan.todoapp;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.firebase.client.ChildEventListener;
+import com.firebase.client.DataSnapshot;
+import com.firebase.client.Firebase;
+import com.firebase.client.FirebaseError;
+import com.firebase.client.ValueEventListener;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        // Get ListView object from xml
+        final ListView listView = (ListView) findViewById(R.id.listView);
+
+        // Create a new Adapter
+        final ArrayAdapter<String> adapter = new ArrayAdapter<>(this,
+                android.R.layout.simple_list_item_1, android.R.id.text1);
+
+        // Assign adapter to ListView
+        listView.setAdapter(adapter);
+
+        // Use Firebase to populate the list.
+        Firebase.setAndroidContext(this);
+
+        // Add items via the Button and EditText at the bottom of the window.
+        final EditText text = (EditText) findViewById(R.id.todoText);
+        final Button button = (Button) findViewById(R.id.addButton);
+        button.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                new Firebase("https://flickering-fire-7492.firebaseio.com/todoItems")
+                        .push()
+                        .child("text")
+                        .setValue(text.getText().toString());
+            }
+        });
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+
+            public void onItemClick(AdapterView<?> parent, View view,
+                                    int position, long id) {
+                new Firebase("https://flickering-fire-7492.firebaseio.com/todoItems")
+                        .orderByChild("text")
+                        .equalTo((String) listView.getItemAtPosition(position))
+                        .addListenerForSingleValueEvent(new ValueEventListener() {
+                            public void onDataChange(DataSnapshot dataSnapshot) {
+                                if (dataSnapshot.hasChildren()) {
+                                    DataSnapshot firstChild = dataSnapshot.getChildren().iterator().next();
+                                    firstChild.getRef().removeValue();
+                                }
+                            }
+
+                            public void onCancelled(FirebaseError firebaseError) {
+                            }
+                        });
+            }
+        });
+
+
+        new Firebase("https://flickering-fire-7492.firebaseio.com/todoItems")
+                .addChildEventListener(new ChildEventListener() {
+                    public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+                        adapter.add((String) dataSnapshot.child("text").getValue());
+                    }
+
+                    public void onChildRemoved(DataSnapshot dataSnapshot) {
+                        adapter.remove((String) dataSnapshot.child("text").getValue());
+                    }
+
+                    public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+                    }
+
+                    public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+                    }
+
+                    public void onCancelled(FirebaseError firebaseError) {
+                    }
+                });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
